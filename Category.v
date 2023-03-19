5faa7aed24aa56ac5df5f73f08592214aa1bb146
remzi <13716567376yh@gmail.com>
2023-03-19 19:47:38 +0800

    composition associativity
    Signed-off-by: remzi <13716567376yh@gmail.com>

--- Category.v
+++ Category.v
@@ -2,6 +2,9 @@ From Coq Require Import Logic.FunctionalExtensionality.
 
 Definition Object: Type := Type.
 
+(* ‘singleton’ set, a set with exactly one element. *)
+Definition Singleton: Object := True.
+
 Definition Morphism (domain: Object) (codomain: Object): Type:= domain -> codomain.
 
 (* All Objects has an identity morphism *)
@@ -14,10 +17,17 @@ Definition composition {domain: Object} {mid: Object} {codomain: Object}
   := fun x => g (f x).
 
 (** The identity function is a unit in the composition *)
-Theorem identity_law_a: forall (a b: Object) (f: Morphism a b),
-  composition (identity a) f = f /\ composition f (identity b) = f.
+Theorem identity_law: forall (A B: Object) (f: Morphism A B),
+  composition (identity A) f = f /\ composition f (identity B) = f.
 Proof.
   unfold composition. unfold identity. intros.
   split; apply functional_extensionality; intros;
   reflexivity.
+Qed.
+
+(** h o (g o f) = (h o g) o f *)
+Theorem associative_law: forall (A B C D: Object) (f: Morphism A B) (g: Morphism B C) (h: Morphism C D),
+  composition (composition f g) h = composition f (composition g h).
+Proof.
+  intros. unfold composition. reflexivity.
 Qed.
